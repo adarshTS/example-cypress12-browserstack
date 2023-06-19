c1dc76fabce0cef3d594d87f4cdc89dc5cd17943
adarshTS <adarsh.ts8@gmail.com>
2023-06-19 21:14:46 +0530

    added ventura OSX

--- cypress/e2e/local.cy.js
+++ cypress/e2e/local.cy.js
@@ -1,6 +1,7 @@
 describe("localdemo", () => {
-    it("localdemo", () => {
-      cy.visit("http://127.0.0.1:5500/");
-      cy.xpath("//a[text()='Designers']").click();
-    });
+  it("localdemo", () => {
+    cy.visit("http://127.0.0.1:5500/");
+    cy.get('[href="#designers"]').click();
+    cy.get('[href="#contact"]').click();
   });
+});
